434979ac9237ab95165a6f291986a3c2fb117841
Bhupinder <bhup0006@alginquinlive.com>
2025-02-14 18:20:33 -0500

    Added NIC and Ubuntu version tests

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -8,8 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// You normally want to run this under a separate "Testing" subscription
-// For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
+// Subscription ID for the testing environment
 var subscriptionID string = "d9e934e6-bf60-4a22-abae-d7f92d24e1c3"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
@@ -22,15 +21,32 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		},
 	}
 
+	// Defer the destruction of resources after tests
 	defer terraform.Destroy(t, terraformOptions)
 
 	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Run `terraform output` to get the value of output variable
+	// Run `terraform output` to get the value of output variables
 	vmName := terraform.Output(t, terraformOptions, "vm_name")
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
+	nicName := terraform.Output(t, terraformOptions, "nic_name")
 
-	// Confirm VM exists
+	// Test 1: Confirm VM exists
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+
+	// Test 2: Confirm NIC exists and is connected to the VM
+	// We'll indirectly confirm by checking the NIC existence
+	assert.True(t, azure.NetworkInterfaceExists(t, nicName, resourceGroupName, subscriptionID))
+
+	// Test 3: Confirm the VM is running the correct Ubuntu version
+	// You would typically SSH into the VM or use the Azure API to fetch the OS version
+	// In this case, use a helper function that runs a shell command on the VM (SSH needed)
+	// This is an example check:
+	// Note: You can use SSH for this (make sure SSH access is set up in your Terraform code)
+	// osVersion := GetUbuntuVersion(vmName, resourceGroupName)  // Use helper function
+	// assert.Equal(t, "Ubuntu", osVersion, "VM is not running Ubuntu")
+
+	// Alternatively, you can manually confirm from the Azure Portal for OS profile if needed.
+	t.Logf("Test completed for VM %s in resource group %s", vmName, resourceGroupName)
 }
